4ebf78545e4fb6a986b3486aec256273a421c3b5
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2025-09-20 14:53:30 +0200

    - Fix typos in Taskfile - Update config server POM version -  data.sql for multiple profiles

--- configserver/src/main/resources/data.sql
+++ configserver/src/main/resources/data.sql
@@ -1,5 +1,9 @@
 INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, "KEY", "VALUE") VALUES
-('Product Service', 'default', 'master', 'spring.datasource.url', 'jdbc:postgresql://postgres:5432/odins_oddities'),
-('Product Service', 'default', 'master', 'spring.datasource.username', '${POSTGRES_USER}'),
-('Product Service', 'default', 'master', 'spring.datasource.password', '${POSTGRES_PASSWORD}'),
-('Product Service', 'default', 'master', 'spring.jpa.hibernate.ddl-auto', 'update');
+('Product Service', 'k8s', 'master', 'spring.datasource.url', 'jdbc:postgresql://postgres:5432/odins_oddities'),
+('Product Service', 'k8s', 'master', 'spring.datasource.username', '${POSTGRES_USER}'),
+('Product Service', 'k8s', 'master', 'spring.datasource.password', '${POSTGRES_PASSWORD}'),
+('Product Service', 'k8s', 'master', 'spring.jpa.hibernate.ddl-auto', 'update'),
+('Product Service', 'local', 'master', 'spring.datasource.url', 'jdbc:postgresql://localhost:5432/odins_oddities'),
+('Product Service', 'local', 'master', 'spring.datasource.username', '${POSTGRES_USER}'),
+('Product Service', 'local', 'master', 'spring.datasource.password', '${POSTGRES_PASSWORD}'),
+('Product Service', 'local', 'master', 'spring.jpa.hibernate.ddl-auto', 'create');
